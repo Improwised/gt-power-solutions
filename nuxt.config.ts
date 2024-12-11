fa63602a57b2569458f05bfb73ba65745bdee49f
husen kureshi <husen.kureshi@improwised.com>
2024-12-11 19:03:57 +0530

    add nuxt boilerplate

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  runtimeConfig: {
+    public: {
+      base_url: process.env.BASE_URL,
+    },
+  },
+  app: {
+    head: {
+      title: "gt-power-solutions",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Poppins",
+        },
+      ],
+    },
+  },
+
+  vue: {
+    propsDestructure: true,
+  },
+  css: ["bootstrap/dist/css/bootstrap.min.css", "~/assets/css/style.css"],
+  modules: [
+    "@nuxt/content",
+    "@pinia/nuxt",
+    "@nuxt/image",
+    "@nuxt/test-utils/module",
+  ],
+});
